87ac12e55f2560bb752f0af52de3fe3eb50ef8fc
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-07-17 20:15:58 -0400

    Fix api routes after rename

--- routes/api/index.js
+++ routes/api/index.js
@@ -8,36 +8,37 @@ router.get('/ok', function (req, res) {
   res.send('ok')
 })
 
+router.all('/echo', function (req, res) {
+  res.json(req.body)
+})
+
 router.get('/users', function (req, res) {
   db.User.findAll()
-    .then(dbUser => res.json(dbUser))
+    .then(dbUsers => res.json(dbUsers))
 })
 
 router.post('/users', function (req, res) {
-  newUser = {...req.body}
+  newUser = { ...req.body }
   db.User.create(newUser)
     .then(dbUser => res.json(dbUser))
 })
 
-router.get('/events', function (req, res) {
-  db.Event.findAll()
-    .then(dbEvent => res.json(dbEvent))
+router.get('/potluck', function (req, res) {
+  db.Potluck.findAll({ include: [{ all: true }] })
+    .then(dbPotluck => res.json(dbPotluck))
 })
 
-router.post('/events', function (req, res) {
-  newEvent = {...req.body}
-  db.Event.create(newEvent)
-    .then(dbEvent => res.json(dbEvent))
+router.post('/potluck', function (req, res) {
+  newPotluck = { ...req.body }
+  db.Potluck.create(newPotluck)
+    .then(dbPotluck => res.json(dbPotluck))
 })
 
-router.get('/events/:eventId', function (req, res) {
-  db.Event.findAll({where: {id: req.params.eventId}})
-  .then(dbEvent => res.json(dbEvent))
+router.get('/potluck/:potluckId', function (req, res) {
+  db.Potluck.findAll({ where: { id: req.params.potluckId }, include: [{ all: true }] })
+    .then(dbPotluck => res.json(dbPotluck))
 })
 
-router.all('/echo', function (req, res) {
-  res.json(req.body)
-})
 
 
 module.exports = router;
