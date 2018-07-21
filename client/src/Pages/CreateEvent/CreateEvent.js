0c18c141cab193dbe97dca14361582f15d830a0e
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-07-21 11:13:33 -0400

    Switch back to Select component

--- client/src/Pages/CreateEvent/CreateEvent.js
+++ client/src/Pages/CreateEvent/CreateEvent.js
@@ -2,50 +2,50 @@ import React, { Component } from "react";
 import Jumbotron from '../../components/Jumbotron'
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
+import { Input, TextArea, FormBtn, Select } from "../../components/Form";
 import { Link as ClickyThing } from "react-router-dom";
 import API from '../../Utils/API'
 import Navbar from '../../components/NavBar'
 
 class CreateEvent extends Component {
 
-    state = {
-        events: [],
-				userData: [],
-				currentUser: "",
-        potluckName: "",
-        potluckDate: "",
-        potluckLocation: ""
-    };
+	state = {
+		events: [],
+		userData: [],
+		currentUser: "",
+		potluckName: "",
+		potluckDate: "",
+		potluckLocation: ""
+	};
 
-    handleInputChange = event => {
-        const { name, value } = event.target;
-        
+	handleInputChange = event => {
+		const { name, value } = event.target;
+
 		this.setState({
 			[name]: value
 		});
-		};
-		
-		// handleSelectChange(event) {
-		// 	this.setState({currentUser: event.target.value});
-		// }
+	};
 
-		componentDidMount() {
-			this.loadEvents();
-			};
-	
-    loadEvents = () => {
-			API.getUsers()
-			.then(res => 
-				this.setState({userData: res.data})
+	// handleSelectChange(event) {
+	// 	this.setState({currentUser: event.target.value});
+	// }
+
+	componentDidMount() {
+		this.loadEvents();
+	};
+
+	loadEvents = () => {
+		API.getUsers()
+			.then(res =>
+				this.setState({ userData: res.data })
 			)
-			};
-	
-    handleFormSubmit = event => {
+	};
+
+	handleFormSubmit = event => {
 		event.preventDefault();
 		if (this.state.potluckName && this.state.potluckDate && this.state.potluckLocation) {
 			API.savePotluck({
-			// API.echo({
+				// API.echo({
 				OwnerId: +this.state.currentUser,
 				eventName: this.state.potluckName,
 				eventDate: this.state.potluckDate,
@@ -54,33 +54,27 @@ class CreateEvent extends Component {
 				.then(res => this.loadEvents())
 				.catch(err => console.log(err));
 		}
-    };
+	};
 
-    render() {
+	render() {
 		return (
 			<div>
-			<Navbar />
-			<br/>
-			<br/>
-			<Container fluid>
+				<Navbar />
+				<br />
+				<br />
+				<Container fluid>
 					<Col size="md-6">
 						<Jumbotron>
 							<h1>Create a Potluck</h1>
 						</Jumbotron>
 						<form>
-							<div className="form-group">
-								<label for="currentUser">Pick current user</label>
-								<select
-								className="form-control"
-								name="currentUser"
-								id="currentUser"
+							<Select
+								value={this.state.currentUser}
 								onChange={this.handleInputChange}
-								>
-									{this.state.userData.map(user =>
-										<option value={user.id}>{user.firstName} {user.lastName}</option>
-									)}
-								</select>
-							</div>
+								selectLabel="Pick current user"
+								selectName="currentUser"
+								selectData={this.state.userData}
+							/>
 							<Input
 								value={this.state.potluckName}
 								onChange={this.handleInputChange}
@@ -107,7 +101,7 @@ class CreateEvent extends Component {
 							</FormBtn>
 						</form>
 					</Col>
-			</Container>
+				</Container>
 			</div>
 		);
 	}
