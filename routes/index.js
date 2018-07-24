b1775a17be9cf907059a7d1e18a3d98555ce6b84
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-07-24 15:36:42 -0400

    Restore default route

--- routes/index.js
+++ routes/index.js
@@ -45,8 +45,8 @@ router.get("/logout", function (req, res) {
 
 
 // If no API routes are hit, send the React app
-// router.use(function (req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
