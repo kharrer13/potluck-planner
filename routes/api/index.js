5128f82afdbb9812590d7ba14a146c583c0477aa
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-07-31 22:32:49 -0400

    Condense API routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -20,87 +20,102 @@ router.all('/echo', function (req, res) {
 })
 // end dummy routes for testing
 
-router.get('/users', function (req, res) {
-  let query = {};
-  if (req.query.user_id) {
-    query.id = req.query.user_id;
-  }
+router.route('/users')
+  .get(function(req, res) {
+    let query = {};
+    if (req.query.user_id) {
+      query.id = req.query.user_id;
+    }
 
-  db.User.findAll({
-    where: query,
-    include: [db.Potluck]
-    // include: [{ all: true }]
+    db.User.findAll({
+      where: query,
+      include: [db.Potluck],
+      attributes: [
+        'id',
+        'username',
+        'fullName',
+        'email',
+        'isVegan',
+        'isVegetarian',
+        'isMilkFree',
+        'isEggFree',
+        'isPeanutFree',
+        'isTreenutFree',
+        'isFishFree',
+        'isShellfishFree',
+        'isSoyFree',
+        'isWheatFree',
+        'isGlutenFree'
+      ]
+      // include: [{ all: true }]
+    }).then(dbUsers => res.json(dbUsers));
   })
-    .then(dbUsers => res.json(dbUsers))
-})
+  .post(function(req, res) {
+    let newUser = { ...req.body };
+    db.User.create(newUser).then(dbUser => {
+      let { password, ...tempUser } = dbUser.get();
+      tempUser.redirectTo = '/login';
+      res.json(tempUser);
+    });
+  });
 
-router.post('/users', function (req, res) {
-  let newUser = { ...req.body }
-  db.User.create(newUser)
-    .then(dbUser => {
-      let { password, ...tempUser } = dbUser.get()
-      tempUser.redirectTo = '/login'
-      res.json(tempUser)
-    })
-})
+router.route('/potluck')
+  .get(function (req, res) {
+    let query = {};
+    let include;
 
-router.get('/potluck', function (req, res) {
-  let query = {};
-  let include;
-
-  if (req.query.potluck_id) {
-    query.id = req.query.potluck_id;
-    // include = [{ all: true }];
-    include = [
-      {
-        association: 'Attendee',
+    if (req.query.potluck_id) {
+      query.id = req.query.potluck_id;
+      // include = [{ all: true }];
+      include = [
+        {
+          association: 'Attendee',
+          attributes: ['id', 'fullName', 'username']
+        },
+        {
+          association: 'Invitee',
+          attributes: ['id', 'fullName', 'username']
+        },
+        {
+          association: 'Items',
+          attributes: ['id', 'itemName']
+        },
+        {
+          association: 'Owner',
+          attributes: ['id', 'fullName', 'username']
+        }
+      ];
+    } else {
+      include = ['Attendee', 'Items', {
+        association: 'Invitee',
         attributes: ['id', 'fullName', 'username']
-      },
-      'Items', 'Owner'];
-  } else {
-    include = ['Attendee', 'Items']
-  }
-  // db.Potluck.findAll({ include: [{ all: true }] })
-  db.Potluck.findAll({
-    where: query,
-    include
-  })
-    .then(dbPotluck => res.json(dbPotluck))
-})
-router.get('/mypotlucks', function (req, res) {
-  let query = {};
-  let include;
-
-  if (req.user) {
-    query.OwnerId = req.user.id;
-  } else {
-    include = ['Attendee', 'Items']
-  }
-  // db.Potluck.findAll({ include: [{ all: true }] })
-  db.Potluck.findAll({
-    where: query,
-    include
-  })
-    .then(dbPotluck => res.json(dbPotluck))
-})
-
-router.post('/potluck', function (req, res) {
-  let newPotluck = { ...req.body }
-
-  // later get this from req.user
-  let newOwner
-
-  (req.user) ? newOwner = req.user.id : newOwner = false;
-
-  db.Potluck.create(newPotluck)
-    .then(dbPotluck => {
-      newOwner && (dbPotluck.setOwner(newOwner))
-      res.json(dbPotluck)
+      }]
+    }
+    // db.Potluck.findAll({ include: [{ all: true }] })
+    db.Potluck.findAll({
+      where: query,
+      include
     })
-})
+      .then(dbPotluck => res.json(dbPotluck))
+  })
+  .post(function (req, res) {
+    let newPotluck = { ...req.body }
+
+    // later get this from req.user
+    let newOwner
+
+    (req.user) ? newOwner = req.user.id : newOwner = false;
+
+    db.Potluck.create(newPotluck)
+      .then(dbPotluck => {
+        newOwner && (dbPotluck.setOwner(newOwner))
+        res.json(dbPotluck)
+      })
+  })
 
 // DRY this up and/or roll it into the query one above
-router.get('/potluck/:potluckId', function (req, res) {
+router.route('/potluck/:potluckId')
+.get(function (req, res) {
   // db.Potluck.findAll({ where: { id: req.params.potluckId }, include: [{ all: true }] })
   db.Potluck.findAll({
     where: { id: req.params.potluckId },
@@ -108,56 +123,89 @@ router.get('/potluck/:potluckId', function (req, res) {
   })
     .then(dbPotluck => res.json(dbPotluck))
 })
-
-router.put('/potluck/:potluckId', function (req, res) {
+.put(function (req, res) {
   // db.Potluck.findAll({ where: { id: req.params.potluckId }, include: [{ all: true }] })
   // this actually returns the array of rows updated
   db.Potluck.update(req.body, { where: { id: req.params.potluckId } })
     .then(dbPotluck => res.json(dbPotluck))
 })
 
-router.get('/items', function (req, res) {
+router.get('/mypotlucks', function (req, res) {
   let query = {};
-  if (req.query.item_id) {
-    query.id = req.query.item_id;
-  }
-  // db.Item.findAll({ where: req.query, include: [{ all: true }] })
-  // need to validate req.query, e.g. copy just the parameters that make sense and make sure they are ok
-  db.Item.findAll({
+  let include = [
+    {
+      association: 'Attendee',
+      attributes: ['id', 'fullName', 'username']
+    },
+    {
+      association: 'Invitee',
+      attributes: ['id', 'fullName', 'username']
+    },
+    {
+      association: 'Items',
+      attributes: ['id', 'itemName']
+    },
+    {
+      association: 'Owner',
+      attributes: ['id', 'fullName', 'username']
+    }
+  ];
+
+
+  if (req.user) {
+    query.OwnerId = req.user.id;
+    // include = ['Attendee', 'Invitee', 'Items']
+  } 
+  // else {
+  //   // include = ['Attendee', 'Items']
+  // }
+  // db.Potluck.findAll({ include: [{ all: true }] })
+  db.Potluck.findAll({
     where: query,
-    attributes: ["id",
-      "itemName",
-      "isVegan",
-      "isVegetarian",
-      "isMilkFree",
-      "isEggFree",
-      "isPeanutFree",
-      "isTreenutFree",
-      "isFishFree",
-      "isShellfishFree",
-      "isSoyFree",
-      "isWheatFree",
-      "isGlutenFree"]
+    include
   })
-    .then(dbItem => {
-      return res.json(dbItem)
-    })
+    .then(dbPotluck => res.json(dbPotluck))
 })
 
+router.route('/items')
+  .get(function(req, res) {
+    let query = {};
+    if (req.query.item_id) {
+      query.id = req.query.item_id;
+    }
+    // db.Item.findAll({ where: req.query, include: [{ all: true }] })
+    // need to validate req.query, e.g. copy just the parameters that make sense and make sure they are ok
+    db.Item.findAll({
+      where: query,
+      attributes: [
+        'id',
+        'itemName',
+        'isVegan',
+        'isVegetarian',
+        'isMilkFree',
+        'isEggFree',
+        'isPeanutFree',
+        'isTreenutFree',
+        'isFishFree',
+        'isShellfishFree',
+        'isSoyFree',
+        'isWheatFree',
+        'isGlutenFree'
+      ]
+    }).then(dbItem => {
+      return res.json(dbItem);
+    });
+  })
+  .post(function(req, res) {
+    let newItem = { ...req.body };
 
-
-
-router.post('/items', function (req, res) {
-  let newItem = { ...req.body }
-
-  // later get this from req.user
-  let newOwner = (req.user) ? req.user.id : null;
-  newItem.UserId = newOwner;
-  db.Item.create(newItem)
-    .then(dbItem => {
-      res.json(dbItem)
-    })
-})
+    // later get this from req.user
+    let newOwner = req.user ? req.user.id : null;
+    newItem.UserId = newOwner;
+    db.Item.create(newItem).then(dbItem => {
+      res.json(dbItem);
+    });
+  });
 
 router.post('/potluck-item', function (req, res) {
   db.Potluck.findById(req.body.PotluckId)
@@ -194,6 +242,24 @@ router.post('/attend', function (req, res) {
         .then(result => res.json(result))
       } else {
         dbPotluck.addAttendee(req.user.id)
+        .then(result => res.json(result))
+      }
+
+    })
+    .catch(e => res.json(e))
+
+})
+
+router.post('/invite', function (req, res) {
+
+  db.Potluck.findById(req.body.PotluckId)
+    .then((dbPotluck) => {
+      console.log(JSON.stringify(dbPotluck, '', 2))
+      if (!req.body.invited) {
+        dbPotluck.removeInvitee(req.body.UserId)
+        .then(result => res.json(result))
+      } else {
+        dbPotluck.addInvitee(req.body.UserId)
         .then(result => res.json(result))
       }
 
