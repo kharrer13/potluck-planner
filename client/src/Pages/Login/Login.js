6ec8e4d9bd0873c33077e601365d48dced6eff63
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-08-01 01:02:06 -0400

    Restyle Login and Signup with material-ui

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,104 +1,129 @@
-import React, { Component } from "react";
-import Jumbotron from '../../components/Jumbotron'
-import { Col, Row, Container } from "../../components/Grid";
-import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn, Select } from "../../components/Form";
-import { Link as ClickyThing, Redirect } from "react-router-dom";
-import API from '../../Utils/API'
+import React, { Component } from 'react';
+import { Link as ClickyThing, Redirect } from 'react-router-dom';
+
+import API from '../../Utils/API';
+
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200
+  },
+  menu: {
+    width: 200
+  },
+  button: {
+    margin: theme.spacing.unit
+  }
+});
 
 class Login extends Component {
-	state = {
-		events: [],
-		userData: [],
-		username: "",
-		password: "",
-		redirectToReferrer: false,
-		loginFailed: false
-	};
+  state = {
+    username: '',
+    password: '',
+    redirectToReferrer: false,
+    loginFailed: false
+  };
 
-	handleInputChange = event => {
-		const { name, value } = event.target;
-		this.setState({
-			[name]: value
-		});
-	};
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
 
-	handleFormSubmit = event => {
-		event.preventDefault();
-		if (this.state.username && this.state.password) {
-			API.login({
-				// API.echo({
-				username: this.state.username,
-				password: this.state.password
-			})
-				.then(res => {
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.username && this.state.password) {
+      API.login({
+        // API.echo({
+        username: this.state.username,
+        password: this.state.password
+      })
+        .then(res => {
+          // console.log(res)
+          this.props.handleUserChange(res.data);
+          if (res.data.redirectTo) {
+            console.log('redirectTo', res.data.redirectTo);
+            this.setState({ loginFailed: false, redirectToReferrer: true });
+          }
+        })
+        .catch(err => {
+          console.error(err);
+          this.setState({ loginFailed: true });
+        });
+    }
+  };
 
-					// console.log(res)
-					this.props.handleUserChange(res.data)
-					if (res.data.redirectTo) {
-						console.log('redirectTo', res.data.redirectTo)
-						this.setState({ loginFailed: false, redirectToReferrer: true })
-					}
-				})
-				.catch(err => {
-					console.error(err)
-					this.setState({ loginFailed: true })
-				});
-		}
-	};
+  render() {
+    const { classes } = this.props;
 
-	render() {
-		const { from } = this.props.location.state || { from: { pathname: "/profile" } };
-		const { redirectToReferrer } = this.state;
+    const { from } = this.props.location.state || {
+      from: { pathname: '/profile' }
+    };
+    const { redirectToReferrer } = this.state;
 
-		if (redirectToReferrer) {
-			console.log('redirecting to from:', from)
-			return <Redirect to={from} />;
-		}
+    if (redirectToReferrer) {
+      console.log('redirecting to from:', from);
+      return <Redirect to={from} />;
+    }
 
-		return (
-			<div>
-				<Row>
-					<Col size='md-3'>
-						<div>
-						</div>
-					</Col>
-					<Col size="md-6">
-						<Jumbotron>
-							<h1>Log in</h1>
-						</Jumbotron>
-						<form>
-							<Input
-								value={this.state.username}
-								onChange={this.handleInputChange}
-								name="username"
-								placeholder="Username"
-							/>
-							<Input
-								type="password"
-								value={this.state.password}
-								onChange={this.handleInputChange}
-								name="password"
-								placeholder="Password"
-							/>
-							<FormBtn
-								disabled={!(this.state.username && this.state.password)}
-								onClick={this.handleFormSubmit}
-							>
-								Log in
-							</FormBtn>
-							{this.state.loginFailed && 'Login failed'}
-						</form>
-					</Col>
-				</Row>
-			</div>
-		);
-	}
-
+    return (
+      <div className={classes.root}>
+        <Typography variant="headline">Log in</Typography>
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            id="username"
+            label="Username"
+            value={this.state.username}
+            onChange={this.handleInputChange}
+            className={classes.textField}
+            name="username"
+            placeholder="Username"
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            id="password"
+            label="Password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+            name="password"
+            placeholder="Password"
+            className={classes.textField}
+            type="password"
+            fullWidth
+            // autoComplete="current-password"
+            margin="normal"
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            disabled={!(this.state.username && this.state.password)}
+            onClick={this.handleFormSubmit}
+          >
+            Log in
+          </Button>
+          {this.state.loginFailed && 'Login failed'}
+        </form>
+      </div>
+    );
+  }
 }
 // TODO fix select value per https://reactjs.org/docs/forms.html#the-select-tag
 // or the hacky way is to just have another text input to input user id or username
 // and if username, then add to the backend to handle adding user, or look up in frontend.
 // TODO add conditional rendering to user selector
 
-export default Login;
+// export default Login;
+export default withStyles(styles)(Login);
--- client/src/Pages/Signup/Signup.js
+++ client/src/Pages/Signup/Signup.js
@@ -1,158 +1,168 @@
-import React, { Component } from "react";
-import Jumbotron from '../../components/Jumbotron'
-import { Col, Row, Container } from "../../components/Grid";
-import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn, Select } from "../../components/Form";
-import { Link as ClickyThing, Redirect } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link as ClickyThing, Redirect } from 'react-router-dom';
+
 import validator from 'validator';
-import API from '../../Utils/API'
+import API from '../../Utils/API';
+
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200
+  },
+  menu: {
+    width: 200
+  },
+  button: {
+    margin: theme.spacing.unit
+  }
+});
 
 class Signup extends Component {
+  state = {
+    username: '',
+    password: '',
+    email: '',
+    fullName: '',
+    redirectToReferrer: false,
+    redirectTo: false
+  };
 
-	state = {
-		userData: [],
-		username: "",
-		password: "",
-		email: "",
-		fullName: "",
-		redirectToReferrer: false,
-		redirectTo: false
-	};
-
-	/* 	validateFormInput = () => {
-			let newUserData = {
-				username: this.state.username,
-				password: this.state.password,
-				email: this.state.email,
-				fullName: this.state.fullName,
-			}
-	
-		}
-	 */
-	handleInputChange = event => {
-		const { name, value } = event.target;
-
-		this.setState({
-			[name]: value
-		});
-		// this.validateFormInput();
-	};
-
-	// componentDidMount() {
-	// 	this.loadEvents();
-	// };
-
-	// loadEvents = () => {
-	// 	API.getUsers()
-	// 		.then(res =>
-	// 			this.setState({ userData: res.data })
-	// 		)
-	// };
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
 
 
+  handleFormSubmit = event => {
+    event.preventDefault();
 
-	handleFormSubmit = event => {
-		event.preventDefault();
+    // validate all inputs here
+    let isValid = [
+      validator.isAlphanumeric(this.state.username),
+      validator.isEmail(this.state.email)
+      // validator.isAlpha(this.state.fullName)
+    ];
 
-		// validate all inputs here
-		let isValid = [
-			validator.isAlphanumeric(this.state.username),
-			validator.isEmail(this.state.email)
-			// validator.isAlpha(this.state.fullName)
+    console.log('isValid', isValid);
+    console.log('isValid.every', isValid.every(x => x));
 
-		]
+    if (isValid.every(x => x)) {
+      // API.login({
+      API.saveUser({
+        username: this.state.username,
+        password: this.state.password,
+        email: this.state.email,
+        fullName: this.state.fullName
+      })
+        .then(res => {
+          console.log(res);
+          if (res.data.redirectTo) {
+            console.log(res.data.redirectTo);
+            this.setState({
+              redirectToReferrer: true,
+              redirectTo: res.data.redirectTo
+            });
+          }
+        })
+        .catch(err => console.log(err));
+    } else {
+      console.log('validation error');
+    }
+  };
 
-		console.log('isValid', isValid)
-		console.log('isValid.every', isValid.every(x => x))
+  render() {
+    const { classes } = this.props;
 
+    const { from } = this.props.location.state || {
+      from: { pathname: this.state.redirectTo }
+    };
+    const { redirectToReferrer } = this.state;
 
-		if (isValid.every(x => x)) {
-			// API.login({
-			API.saveUser({
-				username: this.state.username,
-				password: this.state.password,
-				email: this.state.email,
-				fullName: this.state.fullName
-			})
-				.then(res => {
-					console.log(res)
-					if (res.data.redirectTo) {
-						console.log(res.data.redirectTo)
-						this.setState({
-							redirectToReferrer: true,
-							redirectTo: res.data.redirectTo
-						})
-					}
-				})
-				.catch(err => console.log(err));
-		} else {
-			console.log('validation error')
-		}
-	};
+    if (redirectToReferrer) {
+      console.log('redirecting to from:', from);
+      return <Redirect to={from} />;
+    }
 
-	render() {
-		const { from } = this.props.location.state || { from: { pathname: this.state.redirectTo } };
-		const { redirectToReferrer } = this.state;
-
-		if (redirectToReferrer) {
-			console.log('redirecting to from:', from)
-			return <Redirect to={from} />;
-		}
-
-		return (
-			<div>
-				<Row>
-					<Col size='md-3'>
-						<div>
-						</div>
-					</Col>
-					<Col size="md-6">
-						<Jumbotron>
-							<h1>Sign up</h1>
-						</Jumbotron>
-						<form>
-							<Input
-								value={this.state.fullName}
-								onChange={this.handleInputChange}
-								name="fullName"
-								placeholder="Name"
-							/>
-							<Input
-								value={this.state.email}
-								onChange={this.handleInputChange}
-								name="email"
-								placeholder="email"
-							/>
-							<Input
-								value={this.state.username}
-								onChange={this.handleInputChange}
-								name="username"
-								placeholder="Username"
-							/>
-							<Input
-								type="password"
-								value={this.state.password}
-								onChange={this.handleInputChange}
-								name="password"
-								placeholder="Password"
-							/>
-							<FormBtn
-								disabled={!(this.state.username && this.state.password)}
-								onClick={this.handleFormSubmit}
-							>
-								Sign up
-							</FormBtn>
-						</form>
-					</Col>
-				</Row>
-			</div>
-		);
-	}
-
+    return (
+      <div className={classes.root}>
+        <Typography variant="headline">Sign up </Typography>
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            id="fullName"
+            label="Name"
+            value={this.state.fullName}
+            onChange={this.handleInputChange}
+            className={classes.textField}
+            name="fullName"
+            placeholder="Name"
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            id="email"
+            label="Email"
+            value={this.state.fullemailName}
+            onChange={this.handleInputChange}
+            className={classes.textField}
+            name="email"
+            placeholder="Email"
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            id="username"
+            label="Username"
+            value={this.state.username}
+            onChange={this.handleInputChange}
+            className={classes.textField}
+            name="username"
+            placeholder="Username"
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            id="password"
+            label="Password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+            name="password"
+            placeholder="Password"
+            className={classes.textField}
+            type="password"
+            fullWidth
+            // autoComplete="current-password"
+            margin="normal"
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            disabled={!(this.state.username && this.state.password)}
+            onClick={this.handleFormSubmit}
+          >
+            Sign up
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
 // TODO fix select value per https://reactjs.org/docs/forms.html#the-select-tag
 // or the hacky way is to just have another text input to input user id or username
 // and if username, then add to the backend to handle adding user, or look up in frontend.
 // TODO add conditional rendering to user selector
 
-export default Signup;
+// export default Signup;
+export default withStyles(styles)(Signup);
