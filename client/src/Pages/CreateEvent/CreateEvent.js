f45c936a15c71ed23136ab6e656f915ceabe5ee6
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-08-01 16:16:13 -0400

    Restyle CreateEvent

--- client/src/Pages/CreateEvent/CreateEvent.js
+++ client/src/Pages/CreateEvent/CreateEvent.js
@@ -1,90 +1,132 @@
-import React, { Component } from "react";
-import Jumbotron from '../../components/Jumbotron'
-import { Col, Row, Container } from "../../components/Grid";
-import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn, Select } from "../../components/Form";
-import { Link as ClickyThing } from "react-router-dom";
-import API from '../../Utils/API'
-import DatePicker from 'react-datepicker';
-import moment from 'moment';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-import 'react-datepicker/dist/react-datepicker.css';
+import API from '../../Utils/API';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import { withStyles } from '@material-ui/core/styles';
+
+import Moment from 'react-moment';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    // maxWidth: 360,
+    backgroundColor: theme.palette.background.paper
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200
+  },
+  button: {
+    margin: theme.spacing.unit
+  }
+});
 
 class CreateEvent extends Component {
+  state = {
+    events: [],
+    userData: [],
+    potluckName: '',
+    potluckDate: '',
+    potluckLocation: '',
+    startDate: '',
+    privateEvent: false
+  };
 
-	state = {
-		events: [],
-		userData: [],
-		potluckName: "",
-		potluckDate: "",
-		potluckLocation: "",
-		startDate: '',
-		privateEvent: false
-	};
+  handleInputChange = event => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    // if (name==='potluckDate') {
+    // 	console.log(target, value, name)
+    // }
+    this.setState({
+      [name]: value
+    });
+  };
+  handleCheckChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
+  componentDidMount() {
+    this.loadEvents();
+  }
 
-	handleInputChange = event => {
-		const target = event.target;
-		const value = target.type === 'checkbox' ? target.checked : target.value;
-		const name = target.name;
+  loadEvents = () => {
+    API.getUsers().then(res => this.setState({ userData: res.data }));
+  };
 
-		this.setState({
-			[name]: value
-		});
-	};
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (
+      this.state.potluckName &&
+      this.state.potluckDate &&
+      this.state.potluckLocation
+    ) {
+      API.savePotluck({
+        // API.echo({
+        eventName: this.state.potluckName,
+        eventDate: this.state.potluckDate,
+        eventLocation: this.state.potluckLocation,
+        privateEvent: this.state.privateEvent
+      })
+        .then(res => this.loadEvents())
+        .catch(err => console.log(err));
 
-	componentDidMount() {
-		this.loadEvents();
-	};
+      this.props.history.push('/events');
+    }
+  };
 
-	loadEvents = () => {
-		API.getUsers()
-			.then(res =>
-				this.setState({ userData: res.data })
-			)
-	};
-	handleChange = (date) => {
-		this.setState({
-			potluckDate: date
-		});
-	}
-	handleFormSubmit = event => {
-		event.preventDefault();
-		if (this.state.potluckName && this.state.potluckDate && this.state.potluckLocation) {
-			API.savePotluck({
-				// API.echo({
-				eventName: this.state.potluckName,
-				eventDate: this.state.potluckDate,
-				eventLocation: this.state.potluckLocation,
-				privateEvent: this.state.privateEvent
-			})
-				.then(res => this.loadEvents())
-				.catch(err => console.log(err));
+  render() {
+    const { classes } = this.props;
 
-			this.props.history.push('/events');
-		}
-	};
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={8} alignItems="center">
+          <Grid item md={12}>
+            <Typography variant="headline">Create a Potluck</Typography>
+          </Grid>
+          <Grid item md={12}>
+            <form
+              className={classes.container}
+              noValidate
+              autoComplete="off"
+              onSubmit={this.handleFormSubmit}
+            >
+              <TextField
+                value={this.state.potluckName}
+                onChange={this.handleInputChange}
+                className={classes.textField}
+                id="potluckName"
+                name="potluckName"
+                label="Potluck Name"
+                placeholder="Event name (required)"
+                fullWidth
+                margin="normal"
+              />
+              {/*  fix this later */}
+              <TextField
+                id="potluckDate"
+                name="potluckDate"
+                label="Potluck Date"
+                type="datetime-local"
+                value={this.state.potluckDate}
+                onChange={this.handleInputChange}
+                className={classes.textField}
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
 
-	render() {
-		return (
-			<div>
-				<Row>
-					<Col size='md-3'>
-						<div>
-						</div>
-					</Col>
-					<Col size="md-6">
-						<Jumbotron>
-							<h1>Create a Potluck</h1>
-						</Jumbotron>
-						<form>
-							<Input
-								value={this.state.potluckName}
-								onChange={this.handleInputChange}
-								name="potluckName"
-								placeholder="Event name (required)"
-							/>
-							<div className="form-group">
-								<DatePicker
+              {/* <DatePicker
 									className="form-control"
 									selected={this.state.potluckDate}
 									onChange={this.handleChange}
@@ -97,45 +139,59 @@ class CreateEvent extends Component {
 									timeCaption="time"
 									todayButton={"Today"}
 									minDate={moment()}
-								/>
-							</div>
+								/> */}
+              {/* </div> */}
 
-							<Input
-								value={this.state.potluckLocation}
-								onChange={this.handleInputChange}
-								name="potluckLocation"
-								placeholder="Location (required)"
-							/>
+              <TextField
+                value={this.state.potluckLocation}
+                onChange={this.handleInputChange}
+                className={classes.textField}
+                id="potluckLocation"
+                name="potluckLocation"
+                label="Potluck Location"
+                placeholder="Location (required)"
+                fullWidth
+                margin="normal"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={this.state.privateEvent}
+                    onChange={this.handleCheckChange('privateEvent')}
+                    value="privateEvent"
+                  />
+                }
+                label="Mark event private?"
+              />
 
-							<label htmlFor="privateEvent">
-								Mark event private?
-						<input
-									id="privateEvent"
-									name="privateEvent"
-									type="checkbox"
-									checked={this.state.privateEvent}
-									onChange={this.handleInputChange}
-								/>
-							</label>
-							<FormBtn
-								disabled={!(this.state.potluckName && this.state.potluckDate && this.state.potluckLocation)}
-								onClick={this.handleFormSubmit}
-							>
-								Submit Potluck
-							</FormBtn>
-						</form>
-						<h4>Acting as {this.props.currentUser.username}</h4>
-
-					</Col>
-				</Row>
-			</div>
-		);
-	}
-
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                className={classes.button}
+                disabled={
+                  !(
+                    this.state.potluckName &&
+                    this.state.potluckDate &&
+                    this.state.potluckLocation
+                  )
+                }
+                onClick={this.handleFormSubmit}
+              >
+                Submit Potluck
+              </Button>
+            </form>
+            <h4>Acting as {this.props.currentUser.username}</h4>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 // TODO fix select value per https://reactjs.org/docs/forms.html#the-select-tag
 // or the hacky way is to just have another text input to input user id or username
 // and if username, then add to the backend to handle adding user, or look up in frontend.
 // TODO add conditional rendering to user selector
 
-export default CreateEvent;
+// export default CreateEvent;
+export default withStyles(styles)(CreateEvent);
