4d417bfdf87f9629fdf19b93ff1f1706bd010386
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-08-11 21:25:44 -0400

    Cleanup, reformat

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+
 import Events from './Pages/Events';
 import Items from './Pages/Items';
 import EventView from './Pages/EventView';
@@ -22,21 +22,21 @@ import API from './Utils/API';
 
 const styles = theme => ({
   root: {
-		// paddingTop: theme.spacing.unit * 20,
-	},
-	mainsheet: {
-	...theme.mixins.gutters(),
-		paddingTop: theme.spacing.unit * 2,
-		paddingBottom: theme.spacing.unit * 2,
-		// textAlign: 'center'
-	}
+    // paddingTop: theme.spacing.unit * 20,
+  },
+  mainsheet: {
+  ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    // textAlign: 'center'
+  }
 });
 
 class App extends Component {
-	state = {
-		currentUser: {
-			id: null,
-			username: null,
+  state = {
+    currentUser: {
+      id: null,
+      username: null,
       isVegan: false,
       isVegetarian: false,
       isMilkFree: false,
@@ -48,119 +48,119 @@ class App extends Component {
       isSoyFree: false,
       isWheatFree: false,
       isGlutenFree: false
-		},
-		loggedIn: false
-	}
-	componentDidMount() {
+    },
+    loggedIn: false
+  }
+  componentDidMount() {
 
-		this.loadCurrentUser();
-	};
-	loadCurrentUser = () => {
-		API.whoami()
-			.then(res => {
-				this.setState({ currentUser: res.data });
-				(res.data.id) ? this.setState({ loggedIn: true }) : this.setState({ loggedIn: false })
-			})
+    this.loadCurrentUser();
+  };
+  loadCurrentUser = () => {
+    API.whoami()
+      .then(res => {
+        this.setState({ currentUser: res.data });
+        (res.data.id) ? this.setState({ loggedIn: true }) : this.setState({ loggedIn: false })
+      })
 
-	};
+  };
 
-	handleUserChange = (theUser) => {
-		console.log('app.handleUserChange called with ' + JSON.stringify(theUser, '', 2))
-		const { loggedIn, redirectTo, ...tempUser } = theUser;
-		this.setState({
-			currentUser: tempUser,
-			redirectTo,
-			loggedIn
-		})
-		// console.log('app.setState called with ' + JSON.stringify({ loggedIn, ...tempUser }, '', 2))
-	}
+  handleUserChange = (theUser) => {
+    console.log('app.handleUserChange called with ' + JSON.stringify(theUser, '', 2))
+    const { loggedIn, redirectTo, ...tempUser } = theUser;
+    this.setState({
+      currentUser: tempUser,
+      redirectTo,
+      loggedIn
+    })
+    // console.log('app.setState called with ' + JSON.stringify({ loggedIn, ...tempUser }, '', 2))
+  }
 
-	render() {
+  render() {
 
-		const { classes } = this.props;
+    const { classes } = this.props;
 
-		return (
-			<Paper className={classes.root}>
-				<Router>
-					<div>
-						<NavBar
-							currentUser={this.state.currentUser}
-							loggedIn={this.state.loggedIn}
-							handleUserChange={this.handleUserChange}
-						/>
-						<br />
-						<br />
-						<Paper className={classes.mainsheet}>
+    return (
+      <Paper className={classes.root}>
+        <Router>
+          <div>
+            <NavBar
+              currentUser={this.state.currentUser}
+              loggedIn={this.state.loggedIn}
+              handleUserChange={this.handleUserChange}
+            />
+            <br />
+            <br />
+            <Paper className={classes.mainsheet}>
 
-							<div>
-								<Switch>
-									<Route exact path="/" component={Home} />
-									<Route exact path="/events" component={Events} />
-									<Route exact path="/events/:event_id"
-										render={(props) =>
-											<EventView
-												{...props}
-												currentUser={this.state.currentUser}
-												loggedIn={this.state.loggedIn}
-											/>}
-									/>
-									<Route exact path="/items" component={Items} />
-									<Route exact path="/items/:item_id"
-										render={(props) =>
-											<ItemView
-												{...props}
-												currentUser={this.state.currentUser}
-												loggedIn={this.state.loggedIn}
-											/>}
-									/>
-									{/* <Route exact path="/events/:item_id" component={ClaimItem} /> */}
-									<Route exact path="/profile"
-										render={(props) =>
-											<Profile
-												{...props}
-												currentUser={this.state.currentUser}
-												loggedIn={this.state.loggedIn}
-												handleUserChange={this.handleUserChange}
-												loadCurrentUser={this.loadCurrentUser}
-											/>}
-									/>
-									<Route exact path="/login"
-										render={(props) =>
-											<Login
-												{...props}
-												currentUser={this.state.currentUser}
-												loggedIn={this.state.loggedIn}
-												handleUserChange={this.handleUserChange}
-											/>}
-									/>
-									<Route exact path="/signup" component={Signup} />
-									<Route exact path="/create_event"
-										render={(props) =>
-											<CreateEvent
-												{...props}
-												currentUser={this.state.currentUser}
-												loggedIn={this.state.loggedIn}
-											/>}
-									/>
-									<Route exact path="/create_item"
-										render={(props) =>
-											<CreateItem
-												{...props}
-												currentUser={this.state.currentUser}
-												loggedIn={this.state.loggedIn}
-											/>}
-									/>
-									<Route component={NotFound} />
-								</Switch>
-							</div>
+              <div>
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/events" component={Events} />
+                  <Route exact path="/events/:event_id"
+                    render={(props) =>
+                      <EventView
+                        {...props}
+                        currentUser={this.state.currentUser}
+                        loggedIn={this.state.loggedIn}
+                      />}
+                  />
+                  <Route exact path="/items" component={Items} />
+                  <Route exact path="/items/:item_id"
+                    render={(props) =>
+                      <ItemView
+                        {...props}
+                        currentUser={this.state.currentUser}
+                        loggedIn={this.state.loggedIn}
+                      />}
+                  />
+                  {/* <Route exact path="/events/:item_id" component={ClaimItem} /> */}
+                  <Route exact path="/profile"
+                    render={(props) =>
+                      <Profile
+                        {...props}
+                        currentUser={this.state.currentUser}
+                        loggedIn={this.state.loggedIn}
+                        handleUserChange={this.handleUserChange}
+                        loadCurrentUser={this.loadCurrentUser}
+                      />}
+                  />
+                  <Route exact path="/login"
+                    render={(props) =>
+                      <Login
+                        {...props}
+                        currentUser={this.state.currentUser}
+                        loggedIn={this.state.loggedIn}
+                        handleUserChange={this.handleUserChange}
+                      />}
+                  />
+                  <Route exact path="/signup" component={Signup} />
+                  <Route exact path="/create_event"
+                    render={(props) =>
+                      <CreateEvent
+                        {...props}
+                        currentUser={this.state.currentUser}
+                        loggedIn={this.state.loggedIn}
+                      />}
+                  />
+                  <Route exact path="/create_item"
+                    render={(props) =>
+                      <CreateItem
+                        {...props}
+                        currentUser={this.state.currentUser}
+                        loggedIn={this.state.loggedIn}
+                      />}
+                  />
+                  <Route component={NotFound} />
+                </Switch>
+              </div>
 
-						</Paper>
-						{/* {this.state.loggedIn && <BottomBar /> } */}
-					</div>
-				</Router>
-			</Paper>
-		);
-	}
+            </Paper>
+            {/* {this.state.loggedIn && <BottomBar /> } */}
+          </div>
+        </Router>
+      </Paper>
+    );
+  }
 }
 
 // export default App;
